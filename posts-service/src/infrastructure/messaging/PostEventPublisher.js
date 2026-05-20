5affd045e31e906350a15506c38f8e2362252c49
PeterSamehShafik <petersameh953@gmail.com>
2026-05-20 05:53:23 +0300

    push .env.example

--- posts-service/src/infrastructure/messaging/PostEventPublisher.js
+++ posts-service/src/infrastructure/messaging/PostEventPublisher.js
@@ -1,24 +1,11 @@
 import { kafka, producer, CompressionTypes } from './kafkaClient.js';
 import { createPostCreatedEvent, validatePostCreatedEvent } from '@myapp/shared-events/events/post-created-event.js';
 
-
-
 class PostEventPublisher {
 
   async publishPostCreated(post) {
     const topic = process.env.POST_CREATED_TOPIC;
     const event = createPostCreatedEvent(post)
-
-    // const event = {
-    //   eventType: process.env.POST_CREATED_TOPIC,
-    //   timestamp: new Date().toISOString(),
-    //   payload: {
-    //     postId: post._id.toString(),
-    //     title: post.title,
-    //     content: post.content,
-    //     createdAt: post.createdAt,
-    //   },
-    // };
 
     validatePostCreatedEvent(event);
 
